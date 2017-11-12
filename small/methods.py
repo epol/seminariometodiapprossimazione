4e224a9b88c1b9b17bdb3a59233e2c6ee4011898
Enrico Polesel <epol@autistici.org>
2017-11-12 11:52:47 +0100

    python: db method

--- small/methods.py
+++ small/methods.py
@@ -56,3 +56,62 @@ def newton_diagnostic(A,tol,maxiter,norm=numpy.linalg.norm):
              'commutativity': commutativity
     }
 
+def db(A,tol,maxiter,norm=numpy.linalg.norm):
+    i = 0
+    X = A
+    Y = numpy.eye(*(A.shape))
+    try:
+        res = norm(numpy.dot(X,X) - A)
+        (res > tol).all()
+    except:
+        logging.exception('Invalid norm function')
+        return None
+    while (res > tol).all() and (i < maxiter):
+        try:
+            Xinv = numpy.linalg.inv(X)
+            Yinv = numpy.linalg.inv(Y)
+        except numpy.linalg.LinAlgError:
+            logging.critical("Singularity reached")
+            break
+        X = 0.5 * (X + Yinv)
+        Y = 0.5 * (Y + Xinv)
+        res = norm( numpy.dot(X,X) - A )
+        i = i+1
+    if (res> tol).all():
+        logging.warning("Method hasn't converged with enough iterations")
+    return X
+
+def db_diagnostic(A,tol,maxiter,norm=numpy.linalg.norm):
+    i = 0
+    X = [ A]
+    Y = [ numpy.eye(*(A.shape)) ]
+    try:
+        res = [ norm(numpy.dot(X,X) - A) ]
+        (res[-1] > tol).all()
+    except:
+        logging.exception('Invalid norm function')
+        return None
+    commutativity = [ norm(numpy.dot(Y[-1],X[-1]) - numpy.dot(X[-1],Y[-1])) ]
+    while (res[-1] > tol).all() and (i < maxiter):
+        try:
+            Xinv = numpy.linalg.inv(X[-1])
+            Yinv = numpy.linalg.inv(Y[-1])
+        except numpy.linalg.LinAlgError:
+            logging.critical("Singularity reached")
+            break            
+        X_new = 0.5* ( X[-1] + Yinv)
+        Y_new = 0.5* ( Y[-1] + Xinv)
+        X.append(X_new)
+        Y.append(Y_new)
+        res.append(norm( numpy.dot(X_new,X_new) - A ))
+        commutativity.append(norm(numpy.dot(Y_new,X_new) - numpy.dot(X_new,Y_new)))
+        i = i+1
+    if (res[-1]> tol).all():
+        logging.warning("Method hasn't converged with enough iterations")
+    return { 'result': X[-1],
+             'approximations': X,
+             'invapproximations': Y,
+             'residues': res,
+             'iterations': i,
+             'commutativity': commutativity
+    }
